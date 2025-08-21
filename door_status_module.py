3db24f8d24b6118578dd889d89a783c6602471ed
lumencroft <lumencroft@gmail.com>
2025-08-21 09:45:10 +0900

    door open

--- door_status_module.py
+++ door_status_module.py
@@ -1,4 +1,9 @@
-call_count = 0
+import cv2
+import numpy as np
+
+ROI_RATIO = (0.00, 0.20, 0.35, 0.65)
+BRIGHTNESS_THRESHOLD = 200
+MIN_AREA_RATIO = 0.30
 
 def get_door_status(frame):
     global call_count
