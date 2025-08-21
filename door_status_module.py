2d75e4fdf121b96dc5c60e677a27d435ef4c113c
lumencroft <lumencroft@gmail.com>
2025-08-21 19:07:09 +0900

    Update door_status_module.py

--- door_status_module.py
+++ door_status_module.py
@@ -5,8 +5,11 @@ ROI_RATIO = (0.00, 0.20, 0.35, 0.65)
 BRIGHTNESS_THRESHOLD = 200
 MIN_AREA_RATIO = 0.30
 
+# 전역 변수 call_count를 0으로 초기화
+call_count = 0
+
 def get_door_status(frame):
-    global call_count
+    global call_count  # 이 함수 내에서 전역 변수 call_count를 사용하겠다고 선언
     call_count += 1
 
     if call_count <= 5:
