35463b96c109438eb0b879af726282bb909371d5
lumencroft <lumencroft@gmail.com>
2025-08-21 09:47:16 +0900

    always stop

--- crowdedness_module.py
+++ crowdedness_module.py
@@ -1,12 +1,5 @@
 import cv2
 
 def get_crowdedness_decision(frame):
-    h, w, _ = frame.shape
-    center_pixel_color = frame[h // 2, w // 2]
-    
-    if center_pixel_color[1] > 100 and center_pixel_color[0] < 100 and center_pixel_color[2] < 100:
-        crowdedness_value = 1
-    else:
-        crowdedness_value = 2
         
-    return crowdedness_value
+    return 2
